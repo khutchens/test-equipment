a2e29db741c83a387da6d75715c0a2dacaea2e6f
Kyle <kyle@muonspace.com>
2023-12-29 14:31:56 -0800

    Add env script

---
+++ env.sh
@@ -0,0 +1,7 @@
+alias lab="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/scpi.py"
+
+export SPD1000X_TCPADDR="10.1.0.6:5025"
+alias lab-dc="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/spd1000x.py"
+
+export SDG1000X_TCPADDR="10.1.0.5:5025"
+alias lab-sig="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/sdg1000x.py"
