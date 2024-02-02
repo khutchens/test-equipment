4da1eb8fc64914651d5b9adf6ea28a2ed215f534
Kyle <kyle@muonspace.com>
2024-02-02 14:02:00 -0800

    Fix env script

--- env.sh
+++ env.sh
@@ -1,10 +1,12 @@
-alias lab="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/scpi.py"
+repo=$0:a:h
+
+alias lab="$repo/venv/bin/python $repo/scpi.py"
 
 export SPD1000X_TCPADDR="10.1.0.6:5025"
-alias lab-dc="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/spd1000x.py"
+alias lab-dc="$repo/venv/bin/python $repo/spd1000x.py"
 
 export SDG1000X_TCPADDR="10.1.0.5:5025"
-alias lab-sig="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/sdg1000x.py"
+alias lab-sig="$repo/venv/bin/python $repo/sdg1000x.py"
 
 export DPO2014_USBDEVICE="/dev/usbtmc0"
-alias lab-scope="$HOME/spd1000x/venv/bin/python $HOME/spd1000x/dpo2014.py"
+alias lab-scope="$repo/venv/bin/python $repo/dpo2014.py"
